0b2d7904904cc0640a661103c392aa989172363f
watabee <watabee.dev@gmail.com>
2020-09-22 10:04:13 +0900

    Create photographer card layout

--- LayoutsCodelab/app/src/main/java/com/codelab/layoutscodelab/MainActivity.kt
+++ LayoutsCodelab/app/src/main/java/com/codelab/layoutscodelab/MainActivity.kt
@@ -2,12 +2,9 @@ package com.codelab.layoutscodelab
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
 import com.codelab.layoutscodelab.ui.LayoutsCodelabTheme
 
 class MainActivity : AppCompatActivity() {
@@ -17,22 +14,9 @@ class MainActivity : AppCompatActivity() {
             LayoutsCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    PhotographerCard()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LayoutsCodelabTheme {
-        Greeting("Android")
     }
 }
---
+++ LayoutsCodelab/app/src/main/java/com/codelab/layoutscodelab/PhotographerCard.kt
@@ -0,0 +1,62 @@
+package com.codelab.layoutscodelab
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.EmphasisAmbient
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProvideEmphasis
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
+import com.codelab.layoutscodelab.ui.LayoutsCodelabTheme
+
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = { })
+            .padding(16.dp)
+    ) {
+        Surface(
+            modifier = Modifier.preferredSize(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image goes here
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .gravity(Alignment.CenterVertically)
+        ) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+            ProvideEmphasis(emphasis = EmphasisAmbient.current.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PhotographerCardPreview() {
+    LayoutsCodelabTheme {
+        PhotographerCard()
+    }
+}
