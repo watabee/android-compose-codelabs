d8aecc60c744e23b708d08ffcc653dc10048a56c
watabee <watabee.dev@gmail.com>
2020-09-24 16:19:28 +0900

    Barrier

--- LayoutsCodelab/app/src/main/java/com/codelab/layoutscodelab/ConstraintLayout.kt
+++ LayoutsCodelab/app/src/main/java/com/codelab/layoutscodelab/ConstraintLayout.kt
@@ -13,22 +13,34 @@ import com.codelab.layoutscodelab.ui.LayoutsCodelabTheme
 fun ConstraintLayoutContent() {
     ConstraintLayout {
 
-        val (button, text) = createRefs()
+        val (button1, button2, text) = createRefs()
 
         Button(
             onClick = { /* Do something */ },
             // Assign reference "button" to the Button composable
             // and constrain it to the top of the ConstraintLayout
-            modifier = Modifier.constrainAs(button) {
+            modifier = Modifier.constrainAs(button1) {
                 top.linkTo(parent.top, margin = 16.dp)
             }
         ) {
-            Text(text = "Button")
+            Text(text = "Button 1")
         }
 
         Text(text = "Text", Modifier.constrainAs(text) {
-            top.linkTo(button.bottom, margin = 16.dp)
+            top.linkTo(button1.bottom, margin = 16.dp)
+            centerAround(button1.end)
         })
+
+        val barrier = createEndBarrier(button1, text)
+        Button(
+            onClick = { /* Do something */ },
+            modifier = Modifier.constrainAs(button2) {
+                top.linkTo(parent.top, margin = 16.dp)
+                start.linkTo(barrier)
+            }
+        ) {
+            Text(text = "Button 2")
+        }
     }
 }
 
